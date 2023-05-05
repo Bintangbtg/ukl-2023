1af162afb407510cc8d014dbb77ad08fc86f6405
Bintang <112920061+Bintangbtg@users.noreply.github.com>
2023-05-05 18:30:22 +0700

    Update AppLaundry.java

--- Laundry/AppLaundry.java
+++ Laundry/AppLaundry.java
@@ -11,58 +11,34 @@ public class AppLaundry {
         static String key;
         
    public static void main(String[] args) {
-            System.out.println("Apakah sebelumnya anda sudah  memiliki akun?(ya/belum)");
-            String jawab = input.nextLine();
-            if(jawab.equalsIgnoreCase("ya")){
+                System.out.println("-Selamat datang di Laundry Saya-");
                 System.out.println("Masukkan ID Anda");
                 int id = input.nextInt()-1;
                 System.out.println("Hi Selamat Datang "+c.getNama(id));
                 System.out.println("ID anda "+(id+1));
-                System.out.println("Jumlah saldo Anda :"+c.getSaldo(id));
+                System.out.println("Jumlah saldo Anda :Rp."+c.getSaldo(id));
                 menu(id);
-        }
-          else{
-                System.out.println("Maap Gabisa :b");
-                System.out.println("Mohon Melakukan Pendaftaran dahulu secara offline \nLokasi kantor diatas tanah dan di bawah Langit :q");
-                /*System.out.println("Masukkan Nama:");
-            String nama = input.next();
-            c.setNama(nama);
-            System.out.println("Masukkan ID yang diinginkan:");
-            int id = input.nextInt();
-            t.setIDClient(id);
-            System.out.println("Masukkan Alamat:");
-            String alamat = input.next();
-            c.setAlamat(alamat);
-            System.out.println("Masukkan Nomor Telepon:");
-            String telepon = input.next();
-            c.setTelepon(telepon);
-            System.out.println("Masukkan saldo yang ingin ditambahkan");
-            int saldo = input.nextInt();
-            c.addSaldo(saldo);
-            System.out.println("Terimakasih telah mendaftar "+nama);
-            System.out.println("ID anda "+id);
-            menu(id);*/
-        }
     }
     public static void tampilLaundry(){
         int n = jenislaundry.size();
         for(int i=0;i<n;i++){
             System.out.println("------------------------");
-            System.out.println("Jenis Laundry :"+jenislaundry.getNamaJenisLaundry(i));
-            System.out.println("Harga :"+jenislaundry.getHarga(i));
-            System.out.println("Durasi :"+jenislaundry.getDurasi(i));
             System.out.println("ID :"+(i+1));
+            System.out.println("Jenis Laundry :"+jenislaundry.getNamaJenisLaundry(i));
+            System.out.println("Harga :Rp."+jenislaundry.getHarga(i));
+            System.out.println("Durasi :"+jenislaundry.getDurasi(i)+"Jam");
         }
     }
 
         public static void menu(int id){
         while(tetap){
-            System.out.println("-- Laundry --");
+            System.out.println("- Laundry -");
             System.out.println("1. List Laundry");
             System.out.println("2. List Petugas");
             System.out.println("3. List Client");
             System.out.println("4. Laundry");
-            System.out.println("5. Exit");
+            System.out.println("5. Top up");
+            System.out.println("6. Exit");
             System.out.print("Pilih menu: ");
             int menu = input.nextInt();
             if(menu==1){
@@ -84,7 +60,7 @@ public class AppLaundry {
                 tampilLaundry();
                 System.out.println("Masukkan Id laundry yang diinginkan");
                 int laundry = input.nextInt()-1;
-                System.out.println("Masukkan berapa banyak yang akan di laundry:");
+                System.out.println("Masukkan berapa banyak yang akan di laundry:   Kg");
                 int banyak = input.nextInt();
                 System.out.println("Jumlah  yang akan di laundry:"+banyak+"kg");
                 t.setBanyak(banyak);
@@ -94,25 +70,35 @@ public class AppLaundry {
                      int sisa = saldo - harga;
                      c.setSaldo(sisa);
                      c.editSaldo(id, sisa);
-                    System.out.println("----------Total----------");
+                    System.out.println("- Total -");
                     System.out.println("Jenis Laundry :"+jenislaundry.getNamaJenisLaundry(laundry));
-                    System.out.println("Total Harga :"+harga);
+                    System.out.println("Total Harga :Rp."+harga);
                     System.out.println("Durasi :"+jenislaundry.getDurasi(laundry)+" Jam");
                     System.out.println("Terimakasih telah menggunkaan jasa kami");
-                    System.out.println("Total Transaksi:"+harga);
-                    System.out.println("Sisa saldo anda adalah:"+sisa);
+                    System.out.println("Total Transaksi:Rp."+harga);
+                    System.out.println("Sisa saldo anda adalah:Rp."+sisa);
                     System.out.println("Ketik apapun dan enter untuk kembali");
                     key = input.next();
                 }
                 else{
-                    System.out.println("Mohon Maaf Saldo anda tidak cukup");
-                    System.out.println("Ketik apapun dan enter untuk kembali");
+                    System.out.println("Mohon Maaf Saldo anda tidak cukup\nMohon anda top up terlebih dahulu melalui fitur Topup\nMakasih ");
+                     System.out.println("Ketik apapun dan enter untuk kembali");
                     key = input.next();
-                }
+            }
             }
             else if(menu==5){
-                System.out.println("Terimakasih telah percaya dengan kami /n Sampai Jumpa Lagi");
-                tetap = false;
+                    System.out.println("-Top up-");
+                    System.out.println("Masukkan Jumlah untuk top up:Rp.");
+                    int Topup = input.nextInt();
+                    int hasil = c.getSaldo(id)+Topup;
+                    c.editSaldo(id, hasil);
+                    System.out.println("Selamat Anda berhasil Topup sebesar:Rp."+Topup+"\nSaldo anda sekarang:Rp."+hasil);
+                    System.out.println("Ketik apapun dan enter untuk kembali");
+                    key = input.next();
+            }
+            else if(menu==6){
+                    System.out.println("Terimakasih telah percaya dengan kami \nSampai jumpa lagi");
+                    tetap = false;
             }
             else{
                 System.out.println("Input yang anda masukkan tidak sesuai");
@@ -121,4 +107,4 @@ public class AppLaundry {
             }
         }
     }
-}
+        }
